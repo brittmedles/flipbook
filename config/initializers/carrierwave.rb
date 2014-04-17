be4172e19f77246a3fdeb82bafd1480d3667046b
Fara Britt Woolf <fbwoolf@gmail.com>
2014-04-17 13:27:27 -0500

    Updated env variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider              => "AWS",
-    :aws_access_key_id     => "AKIAJXYSDJPAOPXBCI4A",
-    :aws_secret_access_key => "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",
+    :aws_access_key_id     => ENV["AWS_ACCESS_KEY_ID"],
+    :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
     :region                => "us-east-1"
   }
   
