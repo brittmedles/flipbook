9fb2c07c7ac7c10d025332ba80beba3ee6507c91
Fara Britt Woolf <fbwoolf@gmail.com>
2014-04-17 13:22:25 -0500

    Connection to dropbox is working

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Flipbook::Application.routes.draw do
    # get '/oauth/connect' => 'users#connect', :as => :connect
    # get '/oauth/callback' => 'users#callback', :as => :callback
    
-   get  "dropbox/auth_start" => 'dropbox#auth_start', :as => :dbstart
-   get  "dropbox/auth_finish" => 'dropbox#auth_finish', :as => :dbfinish
+   get "dropbox/client_chk" => 'dropbox#client_chk', :as => :dbcheck
+   get "dropbox/auth_start" => 'dropbox#auth_start', :as => :dbstart
+   get "dropbox/auth_finish" => 'dropbox#auth_finish', :as => :dbfinish
    
    get 'profile/:id' => 'public#profile', :as => :profile
    
