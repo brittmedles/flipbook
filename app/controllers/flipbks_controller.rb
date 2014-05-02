15d1bba3916b8a1ffdb51cb11c5570f8aa443ab0
Fara Britt Woolf <fbwoolf@gmail.com>
2014-05-01 20:49:40 -0500

    Revert "Before refactor of flipbk controller"
    This reverts commit 09dea44e52c8b44ee8466d0c67193fe066e57121.
    
    Conflicts:
    	app/controllers/flipbks_controller.rb

--- app/controllers/flipbks_controller.rb
+++ app/controllers/flipbks_controller.rb
@@ -3,7 +3,60 @@ class FlipbksController < ApplicationController
   def index
     @user = User.find(session[:user_id])
   end
+
+  def new
+    @user = User.find(session[:user_id])
+    @book = Flipbk.new
+  end
+
+  def create
+    @user = User.find(session[:user_id])
+    @book = Flipbk.new(params[:flipbk])
+    @book.user_id = session[:user_id]
+    
+    if @book.save
+      dir = "#{RAILS_ROOT}/tmp/#{@book.id}/"
+      name = "#{@book.user_id}-#{@book.id}-#{@book.name.gsub(/\s+/, "")}"
+      orders = []
+      
+      if params[:order]
+        params[:order].each do |o|
+          p = o.split("-")
+          orders << p
+        end
+      end
+
+      if params[:photos]      
+        params[:photos].each do |p|
+          photo = Photo.find(p)
+          
+          orders.each do |o|
+            photo.order = o[1] if photo.id == o[0]
+          end
+          
+          photo.flipbk_id = @book.id
+          photo.save
+        end
+      end
   
+      sorted = @book.photos.sort_by & :order
+      sorted.each_with_index do |photo, index| 
+        Dir.mkdir(dir) unless File.exists?(dir)
+        open("#{dir}image#{photo.order}#{photo.id}.png", 'wb') do |file|
+        file << open(photo.url).read
+        end
+      end
+      
+      save_to_s3(@book, dir, name)
+      
+      #FileUtils.remove_dir(dir,true)
+
+      redirect_to(flipbk_path(@book.id))
+    else
+      render "new"
+    end
+  end
+
   def show
     if session[:user_id]
       @user = User.find(session[:user_id])
@@ -19,20 +72,6 @@ class FlipbksController < ApplicationController
       redirect_to(:root)
       end
     end
-  end
-  
-  def new
-    @user = User.find(session[:user_id])
-    @book = Flipbk.new
-  end
-
-  def create
-    @user = User.find(session[:user_id])
-    @book = Flipbk.new(params[:flipbk])
-    @book.user_id = session[:user_id]
-    
-    @book.save
-    redirect_to(user_path(@user.id))
   end
   
   def edit
@@ -59,6 +98,33 @@ class FlipbksController < ApplicationController
     
     book.delete
     redirect_to(:users)
+  end
+
+  # private
+  
+  def save_to_s3(book, dir, name)
+    service = S3::Service.new(
+      :access_key_id     => "AKIAJM6AC663FA3WLTSQ", 
+      :secret_access_key => "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"
+    )
+    
+    speed = @book.speed / 10
+    system("convert -delay #{speed} #{dir}*.png #{dir}#{name}.gif")
+    
+    #sedt dir/flipbook.gif to amazon s3 and then save public url to flipbk
+    
+    bucket = service.buckets.find("flipbook-ocs")
+
+    new_object = bucket.objects.build("#{name}.gif")
+
+    new_object.content = open("#{dir}#{name}.gif")
+
+    new_object.save
+    
+    object = bucket.objects.find("#{name}.gif")
+    @book.url = object.url
+
+    @book.save  
   end
   
 end
